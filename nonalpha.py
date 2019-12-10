d0d8dbb00a0d603e802d88cc37b2ca4f13c27ffc
jajorda2 <35240709+jajorda2@users.noreply.github.com>
2019-12-10 10:43:37 -0500

    Added Python lyric cleaning script
    Cleans lyrics in the worst way possible--but it works quickly.

---
+++ nonalpha.py
@@ -0,0 +1,5 @@
+with open(r'snoop30.txt', 'r') as infile, \
+     open(r'noalpha.txt', 'w') as outfile:
+    data = infile.read()
+    data = data.replace("!", "").replace("@", "").replace("#", "").replace("$", "").replace("%", "").replace("^", "").replace("&", "").replace("*", "").replace("(", "").replace(")", "").replace("_", "").replace("+", "").replace("-", "").replace("=", "").replace("{", "").replace("}", "").replace("[", "").replace("]", "").replace(";", "").replace(":", "").replace(",", "").replace(".", "").replace("<", "").replace(">", "").replace("/", "").replace("?", "").replace("]", "").replace("...", "")
+    outfile.write(data)
