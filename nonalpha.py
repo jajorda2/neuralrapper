876434fae7d1036bb0effba2a81c4abd86d52326
jajorda2 <35240709+jajorda2@users.noreply.github.com>
2019-12-10 10:44:13 -0500

    Update nonalpha.py

--- nonalpha.py
+++ nonalpha.py
@@ -1,4 +1,4 @@
-with open(r'snoop30.txt', 'r') as infile, \
+with open(r'lyrics.txt', 'r') as infile, \
      open(r'noalpha.txt', 'w') as outfile:
     data = infile.read()
     data = data.replace("!", "").replace("@", "").replace("#", "").replace("$", "").replace("%", "").replace("^", "").replace("&", "").replace("*", "").replace("(", "").replace(")", "").replace("_", "").replace("+", "").replace("-", "").replace("=", "").replace("{", "").replace("}", "").replace("[", "").replace("]", "").replace(";", "").replace(":", "").replace(",", "").replace(".", "").replace("<", "").replace(">", "").replace("/", "").replace("?", "").replace("]", "").replace("...", "")
