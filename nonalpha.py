6cb0468abab8519cb421231f5211d60c8dbf8c8e
jajorda2 <35240709+jajorda2@users.noreply.github.com>
2019-12-10 10:44:59 -0500

    Update nonalpha.py

--- nonalpha.py
+++ nonalpha.py
@@ -1,5 +1,5 @@
 with open(r'lyrics.txt', 'r') as infile, \
-     open(r'noalpha.txt', 'w') as outfile:
+     open(r'lyricsclean.txt', 'w') as outfile:
     data = infile.read()
     data = data.replace("!", "").replace("@", "").replace("#", "").replace("$", "").replace("%", "").replace("^", "").replace("&", "").replace("*", "").replace("(", "").replace(")", "").replace("_", "").replace("+", "").replace("-", "").replace("=", "").replace("{", "").replace("}", "").replace("[", "").replace("]", "").replace(";", "").replace(":", "").replace(",", "").replace(".", "").replace("<", "").replace(">", "").replace("/", "").replace("?", "").replace("]", "").replace("...", "")
     outfile.write(data)
